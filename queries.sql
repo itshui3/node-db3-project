7822c08067c7940020f9ff95010550536ab06fc6
Hui <jeffrey.x.hui@gmail.com>
2020-01-22 04:57:22 -0800

    completed queries.sql

--- queries.sql
+++ queries.sql
@@ -2,8 +2,34 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+SELECT c.CategoryID, p.ProductName
+  FROM Products AS p
+JOIN Categories as c
+  ON p.CategoryID = c.CategoryID;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+SELECT o.OrderID, s.ShipperName
+  FROM Orders AS o
+JOIN Shippers as s
+  ON o.ShipperID = s.ShipperID
+WHERE o.OrderDate < '2012-08-09';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+SELECT p.ProductName, od.Quantity
+  FROM OrderDetails AS od
+JOIN Orders as o
+  ON od.OrderID = o.OrderID
+JOIN Products as p
+  ON od.ProductID = p.ProductID
+WHERE od.OrderID = 10251;
+
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+SELECT o.OrderID, c.CustomerName, e.LastName
+  FROM Orders AS o
+JOIN Customers AS c
+  ON o.CustomerID = c.CustomerID
+JOIN Employees AS e
+  ON o.EmployeeID = e.EmployeeID;
