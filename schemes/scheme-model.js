17dc41044a28cd0467b15b6326cebfdfaf256e32
Hui <jeffrey.x.hui@gmail.com>
2020-01-22 13:41:23 -0800

    queries MVP

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,7 +3,7 @@ const db = require('knex')(require('../knexfile').development);
 module.exports = {
   find,
   findById,
-
+  findSteps,
   add,
 
   update,
@@ -14,14 +14,19 @@ module.exports = {
 
 function find() {
   return db('schemes')
-
 }
 
 function findById(id) {
   return db('schemes').where({ id }).first()
 }
 
-
+function findSteps(id) {
+  return db('steps as st')
+    .select('st.id as StepID', 'sc.scheme_name', 'st.step_number', 'st.instructions')
+    .join('schemes as sc', 'st.scheme_id', 'sc.id')
+    .where({ scheme_id: id })
+    .orderBy('st.id', 'asc')
+}
 
 function add(scheme) {
   return db('schemes').insert(scheme)
